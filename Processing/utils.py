702395033ec2acd7d9c45690ba3b4525bd40ed79
Oz Kilim <ozkilim@hotmail.co.uk>
2021-11-23 13:53:28 +0100

    biographsadded

--- Processing/utils.py
+++ Processing/utils.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import matplotlib
 import matplotlib.pyplot as plt
 from networkx.algorithms.community.centrality import girvan_newman
 from networkx.algorithms.community import k_clique_communities
@@ -8,6 +9,7 @@ from sklearn import metrics
 import itertools
 import numpy as np
 import infomap
+import random
 from infomap import Infomap
 
 def presentCommunities(G,communities):
@@ -21,10 +23,34 @@ def presentCommunities(G,communities):
     pos = nx.spring_layout(G)
     nx.draw(G, pos, edge_color='k',  with_labels=False,
             font_weight='light', node_size= 20, width= 0.9)
+    colors = "bgrcmykw"
+    # visualise the communities ground truth by coloring nodes.
+    for idx,c in enumerate(communities):
+        # Make new random color
+        nx.draw_networkx_nodes(G, pos, nodelist=c, node_color=colors[idx],node_size=20)
+        
+    plt.show()
+
+def generate_color():
+    color = '#{:02x}{:02x}{:02x}'.format(*map(lambda x: random.randint(0, 255), range(3)))
+    return color
+
+def presentBioCommunities(G,communities):
+    '''
+    This function takes in a graph object
+    and a list of its communities. It returns
+    a visualisation of the communities each 
+    with its own coloring
+    '''
+
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, edge_color='k',  with_labels=False,
+            font_weight='light', node_size= 20, width= 0.9)
 
     # visualise the communities ground truth by coloring nodes.
-    colors = "bgrcmykw"
     for idx,c in enumerate(communities):
-        nx.draw_networkx_nodes(G, pos, nodelist=c, node_color=colors[idx],node_size=20)
+        # Make new random color
+        color = generate_color()
+        nx.draw_networkx_nodes(G, pos, nodelist=c, node_color=color,node_size=20)
         
     plt.show()
